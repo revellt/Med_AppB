43af2b58c8a780a6379f8eb9ac090f2d7a3dcc02
revellt <zvutozsg@gmail.com>
2024-08-24 01:18:04 -0400

    New files and folders to be added

---
+++ App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'InstantConsultation';
+
+function App() {
+  return (
+    <> </>
+  );
+}
+
+export default App;
+
+// Import necessary modules from React library
+import React, { useEffect } from 'react';
+
+// Import components for routing from react-router-dom library
+import { BrowserRouter, Navbar.js, Routes, Route } from "react-router-dom";
+
+// Import custom Navbar component
+import Navbar from './Navbar/Navbar';
+
+// Function component for the main App
+function App() {
+
+  // Render the main App component
+  return (
+    <div className="App">
+        {/* Set up BrowserRouter for routing */}
+        <BrowserRouter>
+          {/* Display the Navbar component */}
+          <Navbar/>
+
+          {/* Set up the Routes for different pages */}
+          <Routes>
+            {/* Define individual Route components for different pages */}
+          </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+// Export the App component as the default export
+export default App;
---
+++ DoctorCard/DoctorCard.js
@@ -0,0 +1,5 @@
+        <div> <button className='book-appointment-btn'>                    
+            <div>Book Appointment</div>
+          <div>No Booking Fee</div>
+        </button> </div>
+         
